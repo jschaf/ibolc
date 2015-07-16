a14b5e176a8a4764d5e2aa583b63f6badf369dd2
Joe Schafer <joe@jschaf.com>
2015-07-15 22:02:49 -0400

    feat(student): add detail view with links from index

--- ibolc/student/views.py
+++ ibolc/student/views.py
@@ -11,3 +11,9 @@ blueprint = Blueprint('student', __name__, url_prefix='/student',
 @blueprint.route("/", methods=["GET", "POST"])
 def home():
     return render_template("student/home.html", students=Student.query.all())
+
+
+@blueprint.route("/<int:student_id>", methods=["GET"])
+def get_student(student_id):
+    student = Student.get_by_id(student_id)
+    return render_template("student/student.html", student=student)
